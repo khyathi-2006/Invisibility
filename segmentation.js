b9aa84f7e054278fc321a1b0eb9362dd678f7d02
khyathi-20199 <k.khyathi7@gmail.com>
2026-08-02 10:29:18 +0530

    Update segmentation.js

--- segmentation.js
+++ segmentation.js
@@ -1,78 +1,112 @@
 import {
     FilesetResolver,
     ImageSegmenter
-} from "@mediapipe/tasks-vision";
+} from
+"https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22";
 
 
-let segmenter;
+
+let segmenter = null;
 
 
-/*
-    Load MediaPipe Selfie Segmentation
-*/
+
 export async function initializeSegmentation(){
 
+
+    if(segmenter){
+
+        return segmenter;
+
+    }
+
+
+
     const vision =
-        await FilesetResolver.forVisionTasks(
-            "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm"
-        );
+    await FilesetResolver.forVisionTasks(
+
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm"
+
+    );
+
 
 
     segmenter =
-        await ImageSegmenter.createFromOptions(
-            vision,
-            {
-                baseOptions:{
-                    modelAssetPath:
-                    "https://storage.googleapis.com/mediapipe-models/image_segmenter/selfie_segmenter/float16/latest/selfie_segmenter.tflite"
-                },
+    await ImageSegmenter.createFromOptions(
 
-                runningMode:"VIDEO",
+        vision,
 
-                outputCategoryMask:true
-            }
-        );
+        {
+
+            baseOptions:{
+
+                modelAssetPath:
+
+                "https://storage.googleapis.com/mediapipe-models/image_segmenter/selfie_segmenter/float16/1/selfie_segmenter.tflite"
+
+            },
+
+
+            runningMode:"VIDEO",
+
+
+            outputCategoryMask:true
+
+
+        }
+
+    );
+
+
+
+    console.log(
+        "MediaPipe Segmentation Ready"
+    );
 
 
     return segmenter;
+
 
 }
 
 
 
-/*
-    Detect human body mask
-*/
+
+
+
+
 export async function detectPerson(
     segmenter,
     video
 ){
 
+
+    if(!segmenter)
+        return null;
+
+
+
     const result =
-        segmenter.segmentForVideo(
-            video,
-            performance.now()
-        );
+    segmenter.segmentForVideo(
+
+        video,
+
+        performance.now()
+
+    );
 
 
-    const mask =
-        result.categoryMask;
 
+    if(
+        !result.categoryMask
+    ){
 
-    if(!mask){
-
-        return new Float32Array(
-            video.videoWidth *
-            video.videoHeight
-        );
+        return null;
 
     }
 
 
-    const values =
-        mask.getAsFloat32Array();
 
+    return result.categoryMask.getAsFloat32Array();
 
-    return values;
 
 }

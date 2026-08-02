101666b05edf33555a049792cceea222cda06a9a
khyathi-20199 <k.khyathi7@gmail.com>
2026-08-02 10:54:50 +0530

    Update Vite config for base path and build options

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./",
+
   server: {
     host: true,
     port: 5173
   },
-  preview: {
-    host: true,
-    port: 4173
-  },
+
   build: {
     outDir: "dist",
-    emptyOutDir: true
+    sourcemap: true
   }
 });
